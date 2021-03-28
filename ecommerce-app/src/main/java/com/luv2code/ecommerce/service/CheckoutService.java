fa4ded877daa075890385b711329f2882c71bbe6
Shehab El Basmy <71727475+shehabelbasmy@users.noreply.github.com>
2021-03-28 18:42:37 +0200

    Delete ecommerce-app directory

--- ecommerce-app/src/main/java/com/luv2code/ecommerce/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.ecommerce.service;
-
-import com.luv2code.ecommerce.dto.Purchase;
-import com.luv2code.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-
-	public PurchaseResponse placeOrder(Purchase purchase);
-}
